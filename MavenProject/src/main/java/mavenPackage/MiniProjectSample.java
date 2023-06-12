085cb965060b53fa928752c0fb4d0a45b2c57bb0
SnehalS2 <SnehalS2@CTAADFJBQ8D3>
2023-06-12 10:42:16 +0530

    Commit

---
+++ MavenProject/src/main/java/mavenPackage/MiniProjectSample.java
@@ -0,0 +1,18 @@
+package mavenPackage;
+
+public class MiniProjectSample 
+{
+public static void main(String[] args)
+{
+	int a = 11;
+	if(a%2==0)
+	{
+		System.out.println("Number is even");
+	}
+	
+	else
+	{
+	System.out.println("Number is odd");
+    }
+}
+}
